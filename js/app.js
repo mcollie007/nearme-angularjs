3a158e8ea3e82bd3e3950262a6a3c72cf13334c1
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-03-01 15:39:07 -0500

    added routes and templates: main and about.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module('NearMeApp', ['leaflet-directive']);
+var app = angular.module('NearMeApp', ['leaflet-directive', 'ngRoute']);
+
+app.config(function($routeProvider){
+
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'MainController',
+		redirectTo: '/'
+
+	}).when('/about', {
+		controlle: 'AboutController',
+		templateUrl: 'views/about.html'
+	}).otherwise({
+		redirectTo: '/'
+	});
+
+
+});
